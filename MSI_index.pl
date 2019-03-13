0d9ccc151c2002c64df28caa922d535ce3a02efc
VaibhaviNirajPathak <48479768+VaibhaviNirajPathak@users.noreply.github.com>
2019-03-13 10:24:50 +0530

    Update MSI_index.pl

--- MSI_index.pl
+++ MSI_index.pl
@@ -24,26 +24,13 @@ foreach $f_line (@list)
 	$infile2 = $file;
 	$outfile1='';$outfile2='';$outfile3='';$outfile4='';$outfile5='';$outfile6='';$outfile7='';
 	$outfile1 = $file."output.txt";
-	#$outfile2 = $file."temp.txt";
 	$outfile3 = $file."temp1.txt";
-	#$outfile4 = $file."temp2.txt";
 	$outfile5 =  $file."bam_filtered.txt";
-	#$outfile6 = $file."t.txt";
-	#$outfile7 = "printhash.txt";
-
 	$outfile1=~s/\r//g;$outfile5=~s/\r//g;$outfile3=~s/\r//g;
-	#$outfile2=~s/\r//g;$outfile4=~s/\r//g;$outfile6=~s/\r//g;$outfile7=~s/\r//g;
-
 	open(OUTFILE1,">$dirout/$outfile1") or die "cant open outfile1\n";
-	
 	open(OUTFILE3,">$dirout/$outfile3") or die "cant open outfile3\n";
-	#open(OUTFILE4,">$dirout/$outfile4") or die "cant open outfile4\n";
 	open(OUTFILE5,">$dirout/$outfile5") or die "cant open outfile5\n";
-	#open(OUTFILE6,">$dirout/$outfile6") or die "cant open outfile6\n";
-	#open(OUTFILE7,">$dirout/$outfile7") or die "cant open outfile7\n";
-
 	system("intersectBed -abam $dirin/$file -b $infile1 -bed > $dirout/$outfile1");
-
 	$create_fasta = "samtools view $dirin/$file | awk '{OFS=\"\\t\"; print \$1\"\\t\"\$5\"\\t\"\$7\"\\t\"\$11}' -> $dirout/$outfile5";
 	system($create_fasta);
 
@@ -59,15 +46,7 @@ foreach $f_line (@list)
 	($read_id,$read_start,$read_value) = split('\t', $_, 3);
 	$myhash{$read_id}{$read_start} = $read_value;
     }
-=if
-	foreach $reaid (keys %myhash)
-	{
-		foreach $reastart (keys %{$myhash{$reaid}})
-		{
-			print OUTFILE7 "$reaid\t$reastart\t$myhash{$reaid}{$reastart}\n";
-		}
-	}
-=cut
+
 	open(FILE2,"$dirout/$outfile5") or die "cant open infile2\n";
 	@infile2 = <FILE2>;
 	close(FILE2);
@@ -95,7 +74,6 @@ foreach $f_line (@list)
 			if(($data1[0] eq $data2[0]) && ($data1[1] eq $data2[1]) && ($data1[2] eq $data2[2]))
 			{
 				$unique_MSid=$data1[0].":".$data1[1].":".$data1[2];
-					
 				if(($data2[1] >= ($data2[6]-3)) && ($data2[2] <= ($data2[7]+3)))
 				{
 				if(exists($myhash{$r_id}{($data2[6]+1)}))
@@ -106,26 +84,20 @@ foreach $f_line (@list)
 				}
 			}
 		}
-	
 	if(!exists $N_num{$unique_MSid})
 	{
 			$N_num{$unique_MSid}=$N_count;
 	}
-
        }
-
 	foreach $id (keys %N_num)
 	{
 		chomp($id);
 		print OUTFILE2 "$id\t$N_num{$id}\n";
-		
 	}
-
 	open(FILE4,"$dirout/$outfile3") or die "cant open infile3\n";
 	@infile4 = <FILE4>;
 	close(FILE4);
 	%new_hash =();	$value=0;
-
 	foreach $line3 (@infile4)
 	{
 		chomp($line3);
@@ -145,9 +117,7 @@ foreach $f_line (@list)
 		}
 
 		for($i=0;$i<=$#cigar_len;$i++)
-		{
-			
-			if( $cigar_char[$i] eq 'M')
+		{	if( $cigar_char[$i] eq 'M')
 			{
 				$pos = $cigar_len[$i] + $pos;
 			}
@@ -165,14 +135,11 @@ foreach $f_line (@list)
 				last;
 			}
 		}
-		
-
 	$seq = $data3[13];
 	$base =	substr($seq,$offset,1);
 	$homo_len=1;
 	for($i=1;$i<=length($seq);$i++)
 	{
-		
 		$base2 = substr($seq,($offset+$i),1);
 		if($base eq $base2)
 		{
@@ -206,10 +173,8 @@ foreach $f_line (@list)
 	 	{
 		 	$C++;
 			push(@sorted_array,$new_hash{$MS}{$H_len});
-		
-   	 	}
+	 	}
 		$max = max(@sorted_array);
-	
 		for($i=0;$i<=$#sorted_array;$i++)
 		{
 			$NormC = ($sorted_array[$i]*100)/$max;
@@ -221,16 +186,13 @@ foreach $f_line (@list)
 		$k1=$file;
 		if($N_num{$MS} <= '50')
 		{
-			#print "$MS,N-count=$N_num{$MS}\n";
 			$r_count=0;
 		}
 		$final_hash{$k1}{$MS} = $r_count;
 		$MS_hash{$MS}++;
  	}
 	@fl=();$dirin='';$file='';
-	
 }
-
 	my @sets = sort keys %MS_hash;
 	print OUTPUT1 join("\t", q{NAME}, @sets), qq{\n};
 	foreach my $k (sort keys %final_hash) 
